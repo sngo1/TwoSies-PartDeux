fcd80eb6e359682e7f8b19aadaca011af5b78aff
Samantha Ngo <sngo1@stuy.edu>
2017-12-13 12:19:15 -0500

    Close

--- find.js
+++ find.js
@@ -7,26 +7,51 @@ var boxWidth = box.offsetWidth;
 var targetX = boxWidth / 2;
 var targetY = boxHeight / 2;
 
+// Distance from target at any point
+var d;
 
 console.log( "box height: " + boxHeight );
 console.log( "box width: " + boxWidth );
 
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
-  /* YOUR IMPLEMENTATION */
+    console.log("x0: ", x0, "|y0: ", y0, "|x1: ", x1, "|y1: ", y1);
+    var dist = Math.sqrt((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1));
+    console.log("Distance: ", dist);
+    return dist;
 };
 
-
+// Find the distance
 var findIt = function(e) {
-  /* YOUR IMPLEMENTATION */
+    // console.log("X: ", e.x);
+    // console.log("Y: ", e.y);
+    d = distance(targetX, targetY, e.x, e.y);
+    console.log("Distance: ", d);
+    changeColor();
 };
 
-/*
-your OTHER FXNS
-
-*/
+var changeColor = function(){
+    var maxDist = distance(0,0,boxWidth, boxHeight);
+    var percentage = (d/maxDist).toFixed(0);
+    percentage = 70;
+    console.log("BEFORE: ",box);
+    box.setAttribute("background-color", "hsl(0,0%," + percentage + "%)");
+    console.log("AFTER: ",box);
+    return true;
+}
 
 box.addEventListener("mousemove", findIt);
 
-console.log("X: ", e.X);
-console.log("Y: ", e.Y)
+console.log("Testing distance...");
+distance(0,0,3,4); // 5
+distance(0,0,5,12); // 13
+distance(0,0,7,24); // 25
+distance(0,0,0,16); // 16
+distance(0,0,17,0); // 17
+distance(-17,0,17,0); // 34
+distance(0,6,0,-6); // 12
+distance(0,-6,0,6); // 12
+distance(0,-6,0,-6); // 0
+
+distance 
+
